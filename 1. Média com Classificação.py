d9850217bccbf1920a9052372e81bf497a2e6f3a
Matheus Haselein <matheushved@gmail.com~>
2025-06-29 19:12:57 -0300

    teste

---
+++ 1. Média com Classificação.py
@@ -0,0 +1,9 @@
+print("Vamos calcular sua média")
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+Resultado = (nota1 + nota2 + nota3) / 3
+
+print("Resultado", Resultado)
+
